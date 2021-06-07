0667b0e70e38c26faad489ccc0d00449ee946dfb
HyunupKang <upup1227@naver.com>
2021-06-07 22:29:37 +0900

    Nullable형

---
+++ Number38.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace A038_Nullable
+{
+    class Program
+    {
+        // 값 형식에 null 값을 할당할 수 없다.
+        static void Main(string[] args)
+        {
+            Nullable<int> i = null;
+            Console.WriteLine("i=" + i);
+            Console.WriteLine(i.GetValueOrDefault());
+
+            if (i.HasValue)
+                Console.WriteLine(i.Value); // or Console.WriteLine(i)
+            else
+                Console.WriteLine("Null");
+
+            int? x = null;
+            int j = x ?? 0;
+            Console.WriteLine("x = {0}, j = {1}", x, j);
+
+            Console.WriteLine("x >= 10 ? {0}", x >= 10);
+            Console.WriteLine("x < 10 ? {0}", x < 10);
+
+            if (Nullable.Compare<int>(i, j) < 0)
+                Console.WriteLine("i < j");
+            else if (Nullable.Compare<int>(i, j) > 0)
+                Console.WriteLine("i > j");
+            else
+                Console.WriteLine("i = j");
+        }
+    }
+}
