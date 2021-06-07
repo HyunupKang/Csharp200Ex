e3288dec987f8ae73f700e6e4b4997320f32e94a
HyunupKang <upup1227@naver.com>
2021-06-07 22:31:09 +0900

    Nullable형

--- Number38.cs
+++ Number38.cs
@@ -5,6 +5,10 @@ namespace A038_Nullable
     class Program
     {
         // 값 형식에 null 값을 할당할 수 없다.
+        // Nullable형은 언제 필요할 까? 
+        // Null이란 "값이 없다", "정해지지 않았다"라는 의미
+        // 따라서 Nullalble은 데이터베이스에서 자주 사용
+        // 예를  들어 데이터베이스에서 나이를 가져와서 int age에 할당하려고 할떄, 그 컬럼의 값이 null이라면 int에 가져올 수 없당
         static void Main(string[] args)
         {
             Nullable<int> i = null;
